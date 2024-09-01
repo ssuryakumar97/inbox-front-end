627a628eb9f33d18271897e87a0dee51fb6b0a47
surya <ssuryakumar97@gmail.com>
2024-09-01 20:06:26 +0530

    updated

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -215,7 +215,8 @@ const Login = () => {
             <TextDiv>Create a new account</TextDiv>
             <GoogleDiv>
               <FrameDiv>
-                  <Link to="https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=http://localhost:5173/home">
+                  {/* <Link to="https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=http://localhost:5173/home"> */}
+                  <Link to="https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=https://inboxfrontend.netlify.app/home">
                 <InnerDiv >
                   <InnerGoogleLogo src="googleLogo.png" alt="Google" />
                   <InnerText>Sign Up with Google</InnerText>
